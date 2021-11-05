8b149d95aa66944c29bde043c78ff7205c23da9f
Celso Crivelaro <celsocrivelaro@gmail.com>
2021-11-04 22:14:28 -0300

    bisavo

--- 02-recursao.pl
+++ 02-recursao.pl
@@ -4,11 +4,13 @@ mae(cleia, otavio).
 pai(joao, joana).
 pai(orozimbo, otavio).
 pai(otavio, luis).
+pai(serafim, orozimbo).
 irmao(orozimbo, manuel).
 
 % X = avo
 % Y = neto(a)
 avo_masculino(X,Y) :- (mae(W,Y) ; pai(W,Y)), pai(X,W).
+bisavo_masculino(X,Y) :- avo_masculino(W,Y), pai(X,W).
 
 % Posso ter regras recursivas
 % As regras pode ser repetidas, uma delas precisa ser critério de parada
