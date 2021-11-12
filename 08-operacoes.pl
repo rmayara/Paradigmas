62b62456c3cc02d6359f73eee46de561e17e2bfb
Celso Crivelaro <celsocrivelaro@gmail.com>
2021-11-11 22:35:42 -0300

    operacaoes

--- 08-operacoes.pl
+++ 08-operacoes.pl
@@ -1,11 +1,3 @@
-% fatorial
-fatorial(0, 1). 
-fatorial(N, Resultado) :- 
-  N > 0,
-  M is N - 1,
-  fatorial(M, Parcial),
-  Resultado is N * Parcial.
-
 % contando usando recursao e listas
 contar(0, []).
 contar(Contador, [_|Tail]) :- contar(ContadorTail, Tail), Contador is ContadorTail + 1.
