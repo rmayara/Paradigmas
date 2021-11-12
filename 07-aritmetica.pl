dadbd83aab613e169931de21c336a0ff02ccd91f
Celso Crivelaro <celsocrivelaro@gmail.com>
2021-11-11 21:53:32 -0300

    fibonnacci

--- 07-aritmetica.pl
+++ 07-aritmetica.pl
@@ -11,4 +11,13 @@ area(brasil, 8).
 dens(PAIS, DENSIDADE) :-
     populacao(PAIS, P),
     area(PAIS, A),
-    DENSIDADE is P/A.
+    DENSIDADE is P/A. % IS => Faz a conta
+
+% fatorial
+fatorial(0, 1). %  Fato
+% Regra
+fatorial(N, Resultado) :- 
+  N > 0,
+  M is N - 1,
+  fatorial(M, Parcial),
+  Resultado is N * Parcial.
--- exercicios/06-aritmetica.pl
+++ exercicios/06-aritmetica.pl
@@ -1,3 +1,9 @@
+% Fibonnacci
+% fib(N,R) ?
+% F = F-1 + F-2
+% F(1) = 1
+% F(0) = 0
+
 % dada uma lista L de números, como calculo o maximo N?
 % L = [0, 10, 3, -2], N = 10
 % max(N,L). 
